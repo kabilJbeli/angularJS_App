07e6e65635550338686dbec8e3c28adb833e1361
kabilJbeli <43722117+kabilJbeli@users.noreply.github.com>
2019-02-23 12:41:31 +0100

    Add files via upload

--- common/common_module.js
+++ common/common_module.js
@@ -21,18 +21,15 @@
         function link(scope, elem, attrs, ngModel) {
             ngModel.$parsers.push(function (viewValue) {
                 var reg = /^[^`~!@#$%\^&*()_+={}|[\]\\:';"<>?,./1-9]*$/;
-                // if view values matches regexp, update model value
                 if (viewValue.match(reg)) {
                     return viewValue;
                 }
-                // keep the model value as it is
                 var transformedValue = ngModel.$modelValue;
                 ngModel.$setViewValue(transformedValue);
                 ngModel.$render();
                 return transformedValue;
             });
         }
-
         return {
             restrict: 'A',
             require: 'ngModel',
