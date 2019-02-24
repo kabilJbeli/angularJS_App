33ade877b43c3a1772320ed4fa74c4b02b840d3b
kabilJbeli <43722117+kabilJbeli@users.noreply.github.com>
2019-02-24 10:41:55 +0100

    Add files via upload

--- app_module.js
+++ app_module.js
@@ -6,3 +6,21 @@ var app = angular.module('app', [
 	'angularUtils.directives.dirPagination'
 ]);
 
+app.factory('testTaskFactory', ['$http', function ($http) {
+	return {
+		testTaskRequest: $http.get("data/users.json")
+	};
+}]);
+
+app.controller('testTaskCTL', ['$http', '$scope', '$q', '$log', 'testTaskFactory', function ($http, $scope, $q, $log, testTaskFactory) {
+	$log.log("Data biding start");
+	$q.all([
+		testTaskFactory.testTaskRequest
+	]).then(function (data) {
+		$scope.usersData = data[0].data;
+	}).catch(function (data) {
+		$log.error("Data binding failed with:", data.status, data.statusText, data.data);
+	}).finally(function () {
+		$log.log("Finally finished data biding.");
+	});
+}]);
